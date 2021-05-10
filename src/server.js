b4342667b7b7d67a7057d17613c4a5b6664d888b
Ming Hsu <leo1997515@gmail.com>
2021-05-10 15:07:27 +0800

    add rule in eslintrc

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
-import express from 'express'
-import dotenv from 'dotenv'
-dotenv.config()
-const app = express()
-const port = process.env.PORT || 3000
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});

3921125536cafd21b5c3f92d3fcb1b8e5ec186d3
Ming Hsu <leo1997515@gmail.com>
2021-05-15 15:16:44 +0800

    add ws

--- src/server.js
+++ src/server.js
@@ -1,13 +1,26 @@
 import express from 'express';
+import http from 'http';
 import dotenv from 'dotenv';
+import WebSocket from 'ws';
+
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
+const server = http.createServer(app);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', function (ws) {
+  ws.on('message', function (msg) {
+    console.log(`received: ${msg}`);
+    ws.send(msg);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server started on port ${server.address().port} :)`);
 });
